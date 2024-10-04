6de193c1c70a266a8d0cb0bb85d9c12fc8032d22
Pedro Cruz <cruz.pedrovr@outlook.com>
2024-10-04 13:19:17 -0300

    Removido teste de exportação, funcionamento ok

--- src/styles/Footer.ts
+++ src/styles/Footer.ts
@@ -1,189 +1,136 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
- .c-footer {
-  height: 157px;
-  position: relative;
-  width: 375px;
-}
+  .superior {
+    height: 120px;
+    display: flex;
+    flex-direction: row;
 
-.c-footer .todos-os-direitos {
-  align-items: center;
-  background-color: var(--azul-escuro);
-  display: flex;
-  gap: 10px;
-  height: 80px;
-  justify-content: center;
-  left: 0;
-  padding: 10px;
-  position: absolute;
-  top: 77px;
-  width: 375px;
-}
+    .left {
+      display: flex;
+      .logocontainer {
+        font-family: Centaur;
+        letter-spacing: 2.4px;
+        font-size: 30px;
+        display: flex;
+        flex-direction: row;
+        gap: 70px;
+        min-width: 409px;
+        background: var(--Azul-escuro, #1c465f);
+        justify-content: center;
+        align-items: center;
+      }
 
-.c-footer .overlap {
-  height: 78px;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 375px;
-}
+      .triangulo {
+        width: 0;
+        height: 0;
+        border-right: 186px solid transparent;
+        border-bottom: 120px solid #1c465e;
+      }
+      img {
+        height: 52px;
+        width: 72px;
+      }
+    }
 
-.c-footer .footer-superior {
-  align-items: center;
-  display: flex;
-  height: 70px;
-  justify-content: center;
-  left: 0;
-  position: absolute;
-  top: 8px;
-  width: 375px;
-}
+    .redessociais {
+      gap: clamp( 130px, 10vw , 150px);
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      align-items: center;
+      justify-content: center;
+      /* max-width: 900px; */
+      /* padding-right: 190px; */
+      /* justify-content: space-around; */
+    }
+  }
+  .inferior {
+    display: flex;
+    height: 80px;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+    align-self: stretch;
+    width: 100%;
 
-.c-footer .container-logo {
-  align-items: center;
-  background-color: var(--azul-escuro);
-  display: flex;
-  flex-direction: column;
-  gap: 7px;
-  height: 70px;
-  justify-content: center;
-  padding: 30px;
-  position: relative;
-  width: 154px;
-}
+    background: var(--Azul-escuro, #1c465f);
+  }
 
-.c-footer .logo-smatrt-TV {
-  align-items: center;
-  background-color: var(--azul-claro);
-  border-radius: 1000px;
-  display: flex;
-  gap: 10px;
-  height: 28px;
-  justify-content: center;
-  margin-top: -19.5px;
-  padding: 10px;
-  position: relative;
-  width: 28px;
-}
+  //tablet
+  @media (max-width: 991px) {
+    .superior {
+      height: 70px;
+      .left {
+        .logocontainer {
+          min-width: 270px;
+          flex-direction: row-gap;
+          img {
+            width: 56px;
+            height: 36px;
+          }
 
-.c-footer .elementos-logo {
-  height: 17.88px;
-  margin-bottom: -4.94px;
-  margin-left: -16.38px;
-  margin-right: -16.38px;
-  margin-top: -4.94px;
-  position: relative;
-  width: 40.75px;
-}
+          .triangulo {
+            width: 0;
+            height: 0;
+            border-right: 105px solid transparent;
+            border-bottom: 120px solid #1c465e;
+          }
+          .text {
+            font-size: 20px;
+          }
+        }
+      }
+    }
+    .redessociais {
+      margin-right: 95px !important;
+      img {
+        width: 30px;
+        height: 30px;
+      }
+    }
+  }
 
-.c-footer .div {
-  height: 18px;
-  position: relative;
-  width: 41px;
-}
+  //mobile
+  @media (max-width: 767px) {
+    .superior {
+      height: 70px;
+      .left {
+        .logocontainer {
+          flex-direction: column;
+          gap: 0px;
+          min-width: 137px;
+          img {
+            width: 40.5px;
+            height: 28px;
+          }
+          .text {
+            font-size: 12px;
+          }
+        }
+        .triangulo {
+          width: 0;
+          height: 0;
+          border-right: 53px solid transparent;
+          border-bottom: 70px solid #1c465e;
+        }
+      }
+    }
+    .redessociais {
+      /* max-width: 185px; */
+      /* padding-right: 26px !important; */
+      gap: 30px !important;
+      justify-content: space-around !important;
 
-.c-footer .elementos {
-  height: 18px;
-  left: 4px;
-  position: absolute;
-  top: 0;
-  width: 33px;
-}
-
-.c-footer .img {
-  height: 4px;
-  left: 0;
-  position: absolute;
-  top: 7px;
-  width: 4px;
-}
-
-.c-footer .elementos-2 {
-  height: 4px;
-  left: 36px;
-  position: absolute;
-  top: 7px;
-  width: 4px;
-}
-
-.c-footer .text-wrapper {
-  color: #ffffff;
-  font-family: "Centaur-Regular", Helvetica;
-  font-size: 12px;
-  font-weight: 400;
-  letter-spacing: 0.96px;
-  line-height: normal;
-  margin-bottom: -18.5px;
-  position: relative;
-  width: 64px;
-}
-
-.c-footer .element {
-  height: 70px;
-  position: relative;
-  width: 53px;
-}
-
-.c-footer .frame {
-  align-items: center;
-  display: inline-flex;
-  flex: 0 0 auto;
-  position: relative;
-}
-
-.c-footer .frame-2 {
-  align-items: center;
-  display: inline-flex;
-  flex: 0 0 auto;
-  gap: 10px;
-  padding: 10px 16px;
-  position: relative;
-}
-
-.c-footer .icones {
-  height: 24px !important;
-  position: relative !important;
-  width: 24px !important;
-}
-
-.c-footer .instagram-white-instance {
-  background-image: url(../../../static/img/combined-shape.svg) !important;
-  height: 24px !important;
-}
-
-.c-footer .design-component-instance-node {
-  background-image: url(../../../static/img/combined-shape-1.svg) !important;
-  height: 10px !important;
-  left: 7px !important;
-  top: 7px !important;
-  width: 10px !important;
-}
-
-.c-footer .icones-2 {
-  height: 4px !important;
-  left: 3px !important;
-  top: 3px !important;
-  width: 4px !important;
-}
-
-.c-footer .geo-white-instance {
-  background-image: url(../../../static/img/elementos-3.svg) !important;
-  height: 24px !important;
-}
-
-.c-footer .icones-3 {
-  height: 6px !important;
-  left: 9px !important;
-  top: 7px !important;
-  width: 6px !important;
-}
-
-.c-footer .linhasuprod {
-  height: 71px;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 375px;
-}
+      img {
+        width: 24px;
+        height: 24px;
+      }
+    }
+    .inferior {
+      font-size: 12px;
+    }
+  }
 `;
